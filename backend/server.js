c6dff290d53aeca7a1a0c53df0f52b543da35f20
Kammar1006 <gim18.kamil.marczynski@gmail.com>
2024-10-30 20:13:32 +0100

    add connection to db and first query

--- backend/server.js
+++ backend/server.js
@@ -6,25 +6,39 @@ const opt = require('../opt.json');
 const PORT = opt.port;
 
 const express = require('express');
-	const http = require('http');
-	const socketio = require('socket.io');
+const http = require('http');
+const socketio = require('socket.io');
 
-	const db = (opt.db_on) ? require('./db.js') : false;
+const db = (opt.db_on) ? require('./db.js') : false;
+
+const app = express();
+app.use(express.static(`${__dirname}/../frontend`));
+
+const server = http.createServer(app);
+const io = socketio(server, {
+	cookie: true,
+});
+
+const {isAlnum, isEmail, isLen} = require("./strings_analyzer.js");
+
+io.on('connection', (sock) => {
 	
-	const app = express();
-	app.use(express.static(`${__dirname}/../frontend`));
-	
-	const server = http.createServer(app);
-	const io = socketio(server, {
-		cookie: true,
+
+	sock.on("searchByName", (name) => {
+		console.log("1");
+		if(!db) return;
+		console.log("2");
+		if(isAlnum(name)){
+			db.query("SELECT * FROM places WHERE name ='"+name+"'", (err, res) => {
+				console.log("3");
+				console.log(err);
+				console.log(res);
+				sock.emit("places", res);
+			});
+		}
 	});
+});
 
-	let roomsTab = [];
-
-	io.on('connection', (sock) => {
-        
-    });
-
-    server.listen(PORT, () => {
-		console.log("Work");
-	});
+server.listen(PORT, () => {
+	console.log("Work");
+});
