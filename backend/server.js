126d60a46f8a3e19bebab14d76ccb672d95591f2
Kammar1006 <gim18.kamil.marczynski@gmail.com>
2024-10-31 21:20:31 +0100

    add backend user authentications

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@
 
 const opt = require('../opt.json');
 const PORT = opt.port;
+const COOKIE_FLAG = opt.cookie
 
 const express = require('express');
 const http = require('http');
@@ -21,22 +22,158 @@ const io = socketio(server, {
 
 const {isAlnum, isEmail, isLen} = require("./strings_analyzer.js");
 
-io.on('connection', (sock) => {
-	
+let translationTab = [];
+const setCID = (sock) => {
+	//console.log(sock.request.headers.cookie);
+	let cid;
+	if(sock.handshake.headers.cookie == undefined){
+		return false;
+	}
+	let cookie = (sock.handshake.headers.cookie).split(" ");
+	cookie.forEach(element => {
+		if(element.split("=")[0]==COOKIE_FLAG) cid = element.split("=")[1];	
+	});
+	if(cid == undefined || cid == ""){
+		return false;
+	}
+	else{
+		if(cid[cid.length-1] == ";"){
+			cid = cid.substring(0, cid.length-1);
+		}
+	}
+	return cid;
+}
+const setTranslationTab = (cid) => {
+	if(translationTab[cid]==undefined){
+		translationTab[cid]={
+			user_id: -1,
+			db_stats: {},
+		};
+	}
+}
+const hasher = (data) => {
+	return bcrypt.hashSync(data, 10);
+}
+const hashCompare = (data, hash) => {
+	return (bcrypt.compareSync(data, hash));
+}
+const emit_login_data = (sock, db_stats) => {
+	let emit_db_stats = {...db_stats};
+	sock.emit("login", emit_db_stats); 
+}
 
-	sock.on("searchByName", (name) => {
-		console.log("1");
+io.on('connection', (sock) => {
+	let cid = setCID(sock);
+	if(!cid) return;
+	setTranslationTab(cid);
+
+	//login:
+	sock.on("login", (login, pass) => {
 		if(!db) return;
-		console.log("2");
+		if(!(isAlnum(log) && isAlnum(pass))) return;
+
+		db.query("SELECT * FROM users WHERE login ='"+login+"'", (err, res) => {
+			console.log(err);
+			console.log(res);
+			if(res && res.length == 1){
+				if(hashCompare(pass, res[0].pass)){
+					console.log("Pass OKOK");
+					translationTab[cid].user_id = res[0].id;
+					translationTab[cid].db_stats = res[0];
+					console.log(translationTab[cid]);
+					emit_login_data(sock, translationTab[cid].db_stats);
+				}
+				else{
+					console.log("Pass NOTOK");
+				}
+			}
+		});
+		
+	});
+
+	//register:
+	sock.on("register", (login, pass, pass2, email) => {
+		console.log("Register:", login, pass, pass2, email);
+		if(!db) return;
+
+		if(!(isLen(nick, 3, 20) && isAlnum(nick))){
+			console.log("Nick len ERR");
+			return;
+		}
+		
+		if(!(isEmail(email))){
+			console.log("Email is NOT ok");
+			return;
+		}
+
+		if(!(isLen(log, 4, 20) && isAlnum(log))){
+			console.log("Login len ERR");
+			return;
+		}
+
+		if(!(isLen(pass, 8, 50) && isAlnum(pass))){
+			console.log("Pass len ERR");
+			return;
+		}
+
+		if(pass === pass2){
+			console.log("All OK!!");
+
+			db.query("SELECT `login`, `email`, FROM `users` WHERE login='"+login+"' OR email='"+email+"' OR user='"+login+"'", (err, res) => {
+				if(res.length){
+					let a=0, b=0;
+					res.forEach(e => {
+						if(e.email == email) a = 1;
+						if(e.login == login) b = 1;
+					});
+					if(a){
+						console.log("Email is in db");
+						sock.emit("register", "email");
+					} 
+					if(b){
+						console.log("Login is in db");
+						sock.emit("register", "login");
+					} 
+				}
+				else{
+					db.query("INSERT INTO `users` (`id`, `login`, `pass`, `email`) VALUES ('', '"+log+"', '"+hasher(pass)+"', '"+email+"')");
+					sock.emit("register", "register");
+				}
+			});
+		}
+	});
+
+	//logout
+	sock.on("logout", () => {
+		translationTab[cid].db_stats = {};
+		translationTab[cid].user_id = -1;
+		sock.emit("logout");
+	});
+
+	//for all users searching db
+	sock.on("searchByName", (name) => {
+		if(!db) return;
 		if(isAlnum(name)){
 			db.query("SELECT * FROM places WHERE name ='"+name+"'", (err, res) => {
-				console.log("3");
 				console.log(err);
 				console.log(res);
 				sock.emit("places", res);
 			});
 		}
 	});
+
+	//for auth users sth:
+	sock.on("auth_user_only", (some_data) => {
+		if(translationTab[cid].user_id === -1) return;
+		//do sth:
+
+		/*
+			//if in db exist sth like rank/grade, may use only for rank:
+			if(translationTab[cid].db_stats.rank == "Admin"){
+				//do sth
+			}
+		*/
+	})
 });
 
 server.listen(PORT, () => {
